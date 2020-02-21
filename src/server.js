93bba45ebdebb53093cc74088f9e997cfd4728d5
jonatas <jonatas.souza@insidesistemas.com.br>
2020-02-21 16:38:35 -0300

    Adicionando principais pacotes

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const socketio = require("socket.io");
+const http = require("http");
+
+const routes = require("./routes");
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+const connectedUsers = {};
+
+io.on("connection", socket => {
+  const { user_id } = socket.handshake.query;
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+server.listen(process.env.APP_PORT || 3333);
